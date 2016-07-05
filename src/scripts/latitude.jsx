1a0a69855a64068c17c6d0d3de40d0ac59c971e0
Philipp Bock <p@philippbock.de>
2016-07-05 17:42:32 +0200

    Improve continent styling

--- src/scripts/latitude.jsx
+++ src/scripts/latitude.jsx
@@ -16,14 +16,14 @@ import Cities from './data/cities';
 
 const store = createStore(latitudeApp, {
 	continents: [
-		{ name: 'North America', north: 83.5, south: 12.3, left: 0, right: 1 },
-		{ name: 'South America', labelAlign: 'right', north: 12.3, south: -55.7, left: 0, right: 1 },
+		{ name: 'North America', labelAlign: 'right', north: 83.5, south: 12.3, left: 0, right: 1 },
+		{ name: 'South America', north: 12.3, south: -55.7, left: 0, right: 1 },
 
-		{ name: 'Europe', north: 71.13, south: 34.6, left: 1, right: 2 },
-		{ name: 'Africa', labelAlign: 'right', north: 37.3, south: -34.8, left: 1, right: 2 },
+		{ name: 'Europe', labelAlign: 'right', north: 71.13, south: 35.0, left: 1, right: 2 },
+		{ name: 'Africa', north: 35.0, south: -34.8, left: 1, right: 2 },
 
-		{ name: 'Asia', north: 77.7, south: 8.7, left: 2, right: 3 },
-		{ name: 'Oceania', labelAlign: 'right', north: -0.4, south: -47.2, left: 2, right: 3 },
+		{ name: 'Asia', labelAlign: 'right', north: 77.7, south: 4.1, left: 2, right: 3 },
+		{ name: 'Oceania', north: 4.1, south: -47.2, left: 2, right: 3 },
 	],
 	visibleCities: [ 19, 139, 3610, 916, 82, 85, 24, 80, 3, 61, 636, 46, 4, 18, 654, 0, 2, 54, 49, 1, 39,	],
 }, window.devToolsExtension && window.devToolsExtension());
